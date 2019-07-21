a62cf80cc69899b7cf91430a44911973e6d67955
axelbraunschweiger <axelbraunschweiger@gmail.com>
2019-07-21 13:00:48 +0200

    Pascals triangle

--- pascDreieck.pas
+++ pascDreieck.pas
@@ -4,44 +4,71 @@
 program x (input, output) ; 
 
 	type 
-	tRefBinBaum = ^tBinBaum ; 
-	tBinBaum = record 
-				info : integer; 
-				next : tRefBinBaum
+	tRefListe = ^tListe ; 
+	tListe = record 
+				wert : integer; 
+				next : tRefListe
 				end; 
 				
 				
+				
+var
+mylist : tRefListe;
+			
+function listeAufbauen (outRefAnfang : tRefListe) : tRefListe;
 
+	var 
+	Zeiger : tRefListe;
+	Zahl : integer;
 
-
-	function pd (inoutRefAnfang : tRefBinBaum ) : tRefBinBaum; 
+	begin
+	 outRefAnfang := nil; 
+	 write('Zahl eingeben: ');
+	 readln(Zahl);
+	 while Zahl <> 99 do 
+	 begin
+		new(Zeiger); 
+		Zeiger^.wert := Zahl; 
+		Zeiger^.next := outRefAnfang; 
+		outRefAnfang := Zeiger; 
+		write('Zahl eingeben: ');
+		readln(Zahl)
+	 end;
+	 listeAufbauen := outRefAnfang
+	end; 
 	
-		var 
-		Zeiger, 
-		ZeigerHinten : tRefBinBaum; 
-		
-		begin
-		
-		Zeiger := inoutRefAnfang; 
-		ZeigerHinten := inoutRefAnfang;
-		while Zeiger <> nil do 
-		  if Zeiger^.info = 1 then 
-		   Zeiger := Zeiger^.next
-		  else 
-		  begin 
-		   Zeiger^.info := ZeigerHinten^.info + Zeiger^.info;
-		   ZeigerHinten := ZeigerHinten^.next; 
-		   Zeiger := Zeiger^.next
-		   end ;
-		pd := inoutRefAnfang
-		end;
-		
+	
+	
+	
+procedure listeDrucken (inRefAnfang: tRefListe);
+
+	var 
+	Zeiger : tRefListe; 
+	
+	begin
+	Zeiger := inRefAnfang; 
+	while Zeiger <> nil do 
+	 begin
+	  write(Zeiger^.wert, '-->');
+	  Zeiger := Zeiger^.next
+	 end
+	end;
 		
 
 
+
+	function PascalscheDreieck(inRefAnfang: tRefListe): tRefListe;
+	
+	
+	end;
+    
+    
 begin 
 
-
+mylist := nil; 
+mylist := listeAufbauen(mylist); 
+mylist := PascalscheDreieck(mylist); 
+listeDrucken(mylist)
 
 
 end.
